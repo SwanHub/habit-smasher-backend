e742ce8544a10519f8b4a81186ccadf8275a934f
SwanHub <jdprince555@gmail.com>
2019-12-06 21:32:28 -0800

    CORS

--- main.go
+++ main.go
@@ -6,17 +6,20 @@ import (
 	"os"
 
 	"github.com/SwanHub/habit-smasher/routes"
+	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
 )
 
-// Step 4: query db for habits
 // Step 5: Fetch that data from a React App
 
 func main() {
-	r := mux.NewRouter()
+	r := mux.NewRouter().StrictSlash(true)
 	// display all active habits
-	r.HandleFunc("/habits", routes.AllHabits)
-	log.Fatal(http.ListenAndServe(":"+os.Getenv("PORT"), r))
+	r.HandleFunc("/habits", routes.AllHabits).Methods("GET")
+	log.Fatal(http.ListenAndServe(":"+os.Getenv("PORT"), handlers.CORS(
+		handlers.AllowedHeaders([]string{"X-Requested-With", "Content-Type", "Authorization"}),
+		handlers.AllowedMethods([]string{"GET", "POST"}),
+		handlers.AllowedOrigins([]string{"*"}))(r)))
 }
 
 // r.HandleFunc("/habitsSchema", routes.CreateHabitTable)
